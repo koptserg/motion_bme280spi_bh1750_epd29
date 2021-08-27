d5d2b46422ddeaa660f72f0860d321deb4812872
Sergey Koptyakov <52034595+koptserg@users.noreply.github.com>
2021-08-27 12:08:46 +0500

    Fixed clock display, pressure, all data changes 2 seconds
    Fixed clock display, pressure, all data changes 2 seconds after reports in a zigbee

---
+++ Source/version.c
@@ -0,0 +1,18 @@
+
+    #ifndef ZCL_APP_VERSION_H
+    #define ZCL_APP_VERSION_H
+
+    #ifdef __cplusplus
+    extern "C" {
+    #endif
+
+    #include "version.h"
+const uint8 zclApp_DateCode[] = { 16, '2', '5', '/', '0', '8', '/', '2', '0', '2', '1', ' ', '2', '1', ':', '0', '7' };
+const char zclApp_DateCodeNT[] = "25/08/2021 21:07";
+
+    #ifdef __cplusplus
+    }
+    #endif
+
+    #endif /* ZCL_APP_VERSION_H */
+    
